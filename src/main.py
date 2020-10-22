7ea117442f720aaa887441be2835e7b8ffddcabf
Anderson Sales <saycor_13_gpb@hotmail.com>
2020-10-22 09:35:59 -0300

    Training, Test and best model saving

---
+++ src/main.py
@@ -0,0 +1,88 @@
+import cv2, os, sys
+import numpy as np
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense,Activation,Flatten,Dropout
+from keras.layers import Conv2D,MaxPooling2D
+from keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+
+data_path = '../../../Desktop/data'
+categories = os.listdir(data_path)
+labels = [i for i in range(len(categories))]
+
+label_dict = dict(zip(categories, labels))
+
+img_size = 100
+data = [] #x
+target = [] #y
+
+for category in categories:
+    folder_path = os.path.join(data_path, category)
+    img_names = os.listdir(folder_path)
+
+    for img_name in img_names:
+        img_path = os.path.join(folder_path, img_name)
+        img = cv2.imread(img_path)
+
+        try:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Grayscale Images
+            resized = cv2.resize(gray, (img_size, img_size)) # Resize
+            data.append(resized)
+            target.append(label_dict[category])
+
+        except Exception as e:
+            print('Exception:', e)
+
+data = np.array(data)/255.0
+data = np.reshape(data, (data.shape[0], img_size, img_size, 1))
+target = np.array(target)
+
+new_target = np_utils.to_categorical(target)
+np.save('data', data)
+np.save('target', new_target)
+
+data = np.load('data.npy')
+target = np.load('target.npy')
+
+train_data, test_data, train_target, test_target = train_test_split(data, target, test_size=0.2)
+
+model=Sequential()
+
+model.add(Conv2D(200, (3, 3), input_shape=data.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+#The first CNN layer followed by Relu and MaxPooling layers
+
+model.add(Conv2D(100, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#The second convolution layer followed by Relu and MaxPooling layers
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+#Flatten layer to stack the output convolutions from second convolution layer
+model.add(Dense(50, activation='relu'))
+#Dense layer of 64 neurons
+model.add(Dense(2, activation='softmax'))
+#The Final layer with two outputs for two categories
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+checkpoint = ModelCheckpoint('model-{epoch:03d}.model', monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
+history = model.fit(train_data, train_target, epochs=20, callbacks=[checkpoint], validation_split=0.2)
+
+plt.plot(history.history['loss'], 'r', label='training loss')
+plt.plot(history.history['val_loss'], label='validation loss')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+plt.plot(history.history['accuracy'], 'r', label='training accuracy')
+plt.plot(history.history['val_accuracy'], label='validation accuracy')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
